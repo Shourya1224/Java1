6789caa32438d7220041dfefcfadbb4feca16ce7
Shourya1224 <shourya21m@gmail.com>
2025-06-07 23:17:43 +0530

    hello

---
+++ Filter.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class Filter
+{
+    public static void main(String[] args) {
+        ArrayList<Integer> list =new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(6);
+        list.add(9);
+        list.add(2);
+
+          int sum=  list.stream().reduce(0,(a,b)->a<b?a:b);
+          System.out.println(sum);
+    }
+}
