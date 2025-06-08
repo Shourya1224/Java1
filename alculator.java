08b40ccf4643e7d2a8de7b0c13684b0ad5e55869
Shourya1224 <shourya21m@gmail.com>
2025-06-08 23:57:17 +0530

    hello

---
+++ alculator.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+public class alculator {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+         
+         System.out.println("Enter the first number:");
+         int a = in.nextInt();      
+      
+        System.out.println("Enter the second number:");
+        int b = in.nextInt();
+        
+        
+        System.out.println("Enter the operator:");
+        char c =  in.next().charAt(0);
+        switch (c) {
+        
+
+            case '+':
+            System.out.println(a+b);
+               break;
+            case'-':
+            System.out.println(a-b);
+                break;
+            case '*':
+            System.out.println(a*b);
+        
+                default:
+                
+
+                
+                 
+
+
+
+            
+        }
+
+        
+
+
+
+
+    }
+
+        
+
+}
+    
+
