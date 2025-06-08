862b129c070c0b05c6f5e14521843ab6b989ef27
Shourya1224 <shourya21m@gmail.com>
2025-06-08 23:53:28 +0530

    hello

---
+++ TryCatch.java
@@ -0,0 +1,20 @@
+public class TryCatch {
+    public static void main(String[] args) {
+
+        int a=10; int b=2;
+
+        try {
+            int c =a/b;
+            System.out.println(c);
+        }
+        catch (Exception e){
+            System.out.println("we can not divide ");
+            
+        }
+
+        System.out.println("namaste");
+        
+
+
+}
+}
