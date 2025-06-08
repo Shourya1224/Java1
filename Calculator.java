d14a9760811b2950b3274a5f004d622b1425a5f8
Shourya1224 <shourya21m@gmail.com>
2025-06-08 23:58:48 +0530

    hello

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class alculator {
+public class Calculator {
     public static void main(String[] args) {
         
         Scanner in = new Scanner(System.in);
